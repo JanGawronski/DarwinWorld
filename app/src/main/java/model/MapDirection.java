df34ff2dff300ef770aa958bad16651db06b3e0d
Kacper Jakubiak <kajakubiak@student.agh.edu.pl>
2024-12-14 21:00:43 +0100

    unitvectoersd

--- app/src/main/java/model/MapDirection.java
+++ app/src/main/java/model/MapDirection.java
@@ -12,9 +12,30 @@ public enum MapDirection {
     W,
     NW;
 
+    private final static Vector2d[] movementVectors = new Vector2d[]{
+            new Vector2d(0, 1),
+            new Vector2d(1, 1),
+            new Vector2d(1, 0),
+            new Vector2d(1, -1),
+            new Vector2d(0, -1),
+            new Vector2d(-1, -1),
+            new Vector2d(-1, 0),
+            new Vector2d(-1, 1)
+    };
+
     public static MapDirection randomDirection() {
         int chosenOrdinal = ThreadLocalRandom.current().nextInt(0, MapDirection.values().length);
         return MapDirection.values()[chosenOrdinal];
     }
 
+    public Vector2d toMovementVector() {
+        return MapDirection.movementVectors[this.ordinal()];
+    }
+
+    public MapDirection rotated(int rotationSteps) {
+        if(rotationSteps < 0)
+            throw new IllegalArgumentException("Rotation cannot be negative");
+        int newOrdinal = (this.ordinal() + rotationSteps) % 8;
+        return MapDirection.values()[newOrdinal];
+    }
 }
---
+++ app/src/test/java/model/MapDirectionTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MapDirectionTest {
+
+    @Test
+    void toMovementVector() {
+        assertEquals(new Vector2d(0, 1), MapDirection.N.toMovementVector());
+        assertEquals(new Vector2d(1, 1), MapDirection.NE.toMovementVector());
+        assertEquals(new Vector2d(1, 0), MapDirection.E.toMovementVector());
+        assertEquals(new Vector2d(1, -1), MapDirection.SE.toMovementVector());
+        assertEquals(new Vector2d(0, -1), MapDirection.S.toMovementVector());
+        assertEquals(new Vector2d(-1, -1), MapDirection.SW.toMovementVector());
+        assertEquals(new Vector2d(-1, 0), MapDirection.W.toMovementVector());
+        assertEquals(new Vector2d(-1, 1), MapDirection.NW.toMovementVector());
+    }
+
+    @Test
+    void rotated() {
+        assertEquals(MapDirection.N, MapDirection.N.rotated(0));
+        assertEquals(MapDirection.SE, MapDirection.NW.rotated(4));
+        assertEquals(MapDirection.W, MapDirection.NW.rotated(7));
+
+        assertThrows(IllegalArgumentException.class,() -> MapDirection.N.rotated(-1));
+    }
+}
