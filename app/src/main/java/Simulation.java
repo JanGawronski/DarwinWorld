d913658443103cce01857cee1fc36b10bc5684d5
Kacper Jakubiak <kajakubiak@student.agh.edu.pl>
2024-12-15 12:36:18 +0100

    Update Simulation.java

--- app/src/main/java/Simulation.java
+++ app/src/main/java/Simulation.java
@@ -1,2 +1,12 @@
+import model.elements.animal.geneselectors.NextGeneSelector;
+import model.map.WorldMap;
+
 public class Simulation {
+    private final NextGeneSelector selector;
+    private final WorldMap map;
+
+    public Simulation(NextGeneSelector selector, WorldMap map) {
+        this.selector = selector;
+        this.map = map;
+    }
 }
