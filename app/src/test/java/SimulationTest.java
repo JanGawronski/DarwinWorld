d4ca2f090d54afe3f35720b76b710aa6f6e7c939
JanGawronski <jangawronski04@gmail.com>
2024-12-22 17:33:50 +0100

    Added testing for simulation

--- app/src/test/java/SimulationTest.java
+++ app/src/test/java/SimulationTest.java
@@ -3,8 +3,11 @@ import org.junit.jupiter.api.Test;
 import model.AnimalConfigData;
 import model.map.WorldMap;
 import model.elements.grass.generators.ForestedEquators;
+import model.elements.animal.Animal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Method;
 import java.util.HashSet;
@@ -40,13 +43,103 @@ public class SimulationTest {
             assertEquals(0, map.getAnimalsMap().values().stream().mapToInt(HashSet::size).sum());
 
             Simulation simulation2 = new Simulation(map, config, new ForestedEquators(map), 10, 1, 10, 1);
-            
+
             removeDeadAnimals.invoke(simulation2);
 
             assertEquals(10, map.getAnimalsMap().values().stream().mapToInt(HashSet::size).sum());
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
         }
+    }
 
-        catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+    @Test
+    void eatGrass() {
+        try {
+            Method eatGrass = Simulation.class.getDeclaredMethod("eatGrass");
+            eatGrass.setAccessible(true);
+
+            AnimalConfigData config = new AnimalConfigData(1, 1, 1, true, 1, 1, 1);
+            WorldMap map1 = new WorldMap(10, 10);
+            Simulation simulation1 = new Simulation(map1, config, new ForestedEquators(map1), 100, 1, 10, 1);
+
+            eatGrass.invoke(simulation1);
+
+            assertTrue(map1.getGrasses().size() < 100);
+            assertTrue(map1.getGrasses().size() >= 90);
+
+            assertTrue(map1.getAnimalsMap().values().stream().flatMap(HashSet::stream)
+                    .anyMatch(animal -> animal.getEnergy() > 1));
+
+            WorldMap map2 = new WorldMap(10, 10);
+            Simulation simulation2 = new Simulation(map2, config, new ForestedEquators(map2), 0, 1, 10, 1);
+
+            eatGrass.invoke(simulation2);
+
+            assertEquals(0, map2.getGrasses().size());
+
+            assertFalse(map2.getAnimalsMap().values().stream().flatMap(HashSet::stream)
+                .anyMatch(animal -> animal.getEnergy() > 1));
+
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void breedAnimals() {
+        try {
+            Method breedAnimals = Simulation.class.getDeclaredMethod("breedAnimals");
+            breedAnimals.setAccessible(true);
+
+            AnimalConfigData config = new AnimalConfigData(1, 1, 1, true, 1, 1, 1);
+            WorldMap map1 = new WorldMap(10, 10);
+            Simulation simulation1 = new Simulation(map1, config, new ForestedEquators(map1), 10, 1, 101, 1);
+
+            breedAnimals.invoke(simulation1);
+
+            assertTrue(map1.getAnimalsMap().values().stream().flatMap((HashSet<Animal> set) -> set.stream()).count() > 101);
+            assertTrue(map1.getAnimalsMap().values().stream().flatMap((HashSet<Animal> set) -> set.stream()).count() <= 151);
+
+            assertTrue(map1.getAnimalsMap().values().stream().flatMap(HashSet::stream)
+            .anyMatch(animal -> animal.getEnergy() > 1));
+
+            assertTrue(map1.getAnimalsMap().values().stream().flatMap(HashSet::stream)
+            .anyMatch(animal -> animal.getEnergy() == 0));
+
+            long countEnergyGreaterThanOne = map1.getAnimalsMap().values().stream()
+                    .flatMap(HashSet::stream)
+                    .filter(animal -> animal.getEnergy() > 1)
+                    .count();
+            long countEnergyEqualToZero = map1.getAnimalsMap().values().stream()
+                    .flatMap(HashSet::stream)
+                    .filter(animal -> animal.getEnergy() == 0)
+                    .count();
+            assertTrue(2 * countEnergyGreaterThanOne == countEnergyEqualToZero);
+
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void growGrass() {
+        try {
+            Method growGrass = Simulation.class.getDeclaredMethod("growGrass");
+            growGrass.setAccessible(true);
+
+            AnimalConfigData config = new AnimalConfigData(1, 1, 1, true, 1, 1, 1);
+            WorldMap map = new WorldMap(10, 10);
+            Simulation simulation = new Simulation(map, config, new ForestedEquators(map), 10, 50, 10, 1);
+
+            growGrass.invoke(simulation);
+
+            assertEquals(60, map.getGrasses().size());
+
+            growGrass.invoke(simulation);
+
+            assertEquals(100, map.getGrasses().size());
+
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
